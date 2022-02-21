5ce99abe3120fed8413e00e28e93fb7326845d63
doug0102 <shea.wow@hotmail.com>
2022-02-21 16:36:15 -0500

    Small refactor to the logic that found and removed the settings links.

--- src/extensions/walls/WallsApplicationCustomizer.ts
+++ src/extensions/walls/WallsApplicationCustomizer.ts
@@ -64,9 +64,10 @@ export default class WallsApplicationCustomizer
     return userType;
   }
 
-  public _render(){
+  public _render() {
     // set interval
-    this._setSettingsPaneInterval();
+    //this._setSettingsPaneInterval();
+    this._awaitSettingsLoad();
 
     // Site contents page
     if (this.context.pageContext.site.serverRequestPath === "/_layouts/15/viewlsts.aspx") {
@@ -80,147 +81,148 @@ export default class WallsApplicationCustomizer
     }
   }
 
-  // Check for settings pane
-  public async _setSettingsPaneInterval(){
+  public async _awaitSettingsLoad() {
     let interval = setInterval(() => {
-      var settingsPane = document.getElementById('FlexPane_Settings');
+      var settingsButton = document.getElementById('O365_MainLink_Settings');
+      
+      // If the settings button exists, attach listeners and clear this interval
+      if(settingsButton) {
 
-      if(settingsPane) {
-        this._addWalls(settingsPane);
+        // We need to pass scope in so we don't lose context in the _addWalls function
+        var scope = this;
+        var addWalls = this._addWalls;
 
-        // No more searching
+        // Bind the removal of links to the settings pane button click
+        settingsButton.addEventListener("click", function() {
+
+          // Give it a short delay so the settings pane has a chance to initialize
+          setTimeout(function() {
+            addWalls(scope);
+          }, 50);
+        });
+        
         clearInterval(interval);
-        this._setSettingsRemoveInterval();
       }
-    }, 500);
+    }, 100);
   }
 
-  // See if settings pane has been closed
-  public async _setSettingsRemoveInterval(){
-    let interval = setInterval(() => {
-      var settingsPane = document.getElementById('FlexPane_Settings');
+  public async _addWalls(scope) {
 
-      if(!settingsPane) {
-
-        // No more searching
-        clearInterval(interval);
-        this._setSettingsPaneInterval();
-      }
-    }, 600);
-  }
-
-  public async _addWalls(settingsPane) {
-    // Remove options in settings
-    var userType = await this._checkUser();
-    // Add page
-    if (userType != "owner") {
-      var aP = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl +'/_layouts/15/CreateSitePage.aspx"]');
-      if (aP.length > 0) aP[0].remove();
-      aP = settingsPane.querySelectorAll("#SuiteMenu_zz8_MenuItemAddPage");
-      if (aP.length > 0) aP[0].remove();
-    }
-
-    //Add app
-    var aP = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl + '/_layouts/15/appStore.aspx#myApps?entry=SettingAddAnApp"]');
-    if (aP.length > 0) aP[0].remove();
-    aP = settingsPane.querySelectorAll("#SuiteMenu_zz5_MenuItemCreate");
-    if (aP.length > 0) aP[0].remove();
-
-    //Global Navigation
-    var gN = settingsPane.querySelectorAll('a[href="javascript:_spLaunchGlobalNavSettings();"]');
-    if (gN.length > 0) gN[0].remove();
-    gN = settingsPane.querySelectorAll("#GLOBALNAV_SETTINGS_SUITENAVID");
-    if (gN.length > 0) gN[0].remove();
-
-    //Hub settings
-    var hS = settingsPane.querySelectorAll('a[href="javascript:_spLaunchHubSettings();"]');
-    if (hS.length > 0) hS[0].remove();
-    hS = settingsPane.querySelectorAll("#SUITENAV_HUB_SETTINGS");
-    if (hS.length > 0) hS[0].remove();
-
-    //Site settings
-    var sT = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl + '/_layouts/15/settings.aspx"]');
-    if (sT.length > 0) sT[0].remove();
-    sT = settingsPane.querySelectorAll("#SuiteMenu_zz7_MenuItem_Settings");
-    if (sT.length > 0) sT[0].remove();
-
-    // Site permissions
-    var sP = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSitePermissions();"]');
-    if(sP.length > 0) sP[0].remove();
-    sP = settingsPane.querySelectorAll("#SUITENAV_SITE_PERMISSIONS");
-    if (sP.length > 0) sP[0].remove();
-    sP = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SitePermissions");
-    if (sP.length > 0) sP[0].remove();
-
-    // Site information
-    if (userType === "owner") {
-      var sI1 = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
-      var sI2 = settingsPane.querySelectorAll('#SuiteMenu_MenuItem_SiteInformation'); //For site content page
-
-      //Check if on home page or site content page
-      if (Object.keys(sI1).length > 0) {
-        sI = sI1
-      } else if (Object.keys(sI2).length > 0) {
-        sI = sI2
+    var settingsPane = document.getElementById('FlexPane_Settings');
+    
+    if(settingsPane !== null) {
+      // Remove options in settings
+      var userType = await scope._checkUser();
+      // Add page
+      if (userType != "owner") {
+        var aP = settingsPane.querySelectorAll('a[href="' + scope.context.pageContext.web.serverRelativeUrl +'/_layouts/15/CreateSitePage.aspx"]');
+        if (aP.length > 0) aP[0].remove();
+        aP = settingsPane.querySelectorAll("#SuiteMenu_zz8_MenuItemAddPage");
+        if (aP.length > 0) aP[0].remove();
       }
 
-      if (sI.length > 0) {
-        let element: HTMLElement = sI[0] as HTMLElement;
-        element.onclick = () => {
-          window.setTimeout(() => {
-            var siteSettingsPane = document.getElementsByClassName("ms-SiteSettingsPanel-SiteInfo");
-            if (siteSettingsPane.length > 0) {
-              window.setTimeout(() => {
-                var jhs = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-joinHubSite");
-                if (jhs.length > 0) jhs[0].remove();
-              }, 300);
-              var c = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-classification");
-              if (c.length > 0) c[0].remove();
-              var p = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-PrivacyDropdown");
-              if (p.length > 0) p[0].remove();
-              var ht = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-HelpText");
-              if (ht.length > 0) ht[0].remove();
-            }
-          }, 500);
+      //Add app
+      var aP = settingsPane.querySelectorAll('a[href="' + scope.context.pageContext.web.serverRelativeUrl + '/_layouts/15/appStore.aspx#myApps?entry=SettingAddAnApp"]');
+      if (aP.length > 0) aP[0].remove();
+      aP = settingsPane.querySelectorAll("#SuiteMenu_zz5_MenuItemCreate");
+      if (aP.length > 0) aP[0].remove();
+
+      //Global Navigation
+      var gN = settingsPane.querySelectorAll('a[href="javascript:_spLaunchGlobalNavSettings();"]');
+      if (gN.length > 0) gN[0].remove();
+      gN = settingsPane.querySelectorAll("#GLOBALNAV_SETTINGS_SUITENAVID");
+      if (gN.length > 0) gN[0].remove();
+
+      //Hub settings
+      var hS = settingsPane.querySelectorAll('a[href="javascript:_spLaunchHubSettings();"]');
+      if (hS.length > 0) hS[0].remove();
+      hS = settingsPane.querySelectorAll("#SUITENAV_HUB_SETTINGS");
+      if (hS.length > 0) hS[0].remove();
+
+      //Site settings
+      var sT = settingsPane.querySelectorAll('a[href="' + scope.context.pageContext.web.serverRelativeUrl + '/_layouts/15/settings.aspx"]');
+      if (sT.length > 0) sT[0].remove();
+      sT = settingsPane.querySelectorAll("#SuiteMenu_zz7_MenuItem_Settings");
+      if (sT.length > 0) sT[0].remove();
+
+      // Site permissions
+      var sP = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSitePermissions();"]');
+      if(sP.length > 0) sP[0].remove();
+      sP = settingsPane.querySelectorAll("#SUITENAV_SITE_PERMISSIONS");
+      if (sP.length > 0) sP[0].remove();
+      sP = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SitePermissions");
+      if (sP.length > 0) sP[0].remove();
+
+      // Site information
+      if (userType === "owner") {
+        var sI1 = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
+        var sI2 = settingsPane.querySelectorAll('#SuiteMenu_MenuItem_SiteInformation'); //For site content page
+
+        //Check if on home page or site content page
+        if (Object.keys(sI1).length > 0) {
+          sI = sI1
+        } else if (Object.keys(sI2).length > 0) {
+          sI = sI2
         }
+
+        if (sI.length > 0) {
+          let element: HTMLElement = sI[0] as HTMLElement;
+          element.onclick = () => {
+            window.setTimeout(() => {
+              var siteSettingsPane = document.getElementsByClassName("ms-SiteSettingsPanel-SiteInfo");
+              if (siteSettingsPane.length > 0) {
+                window.setTimeout(() => {
+                  var jhs = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-joinHubSite");
+                  if (jhs.length > 0) jhs[0].remove();
+                }, 300);
+                var c = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-classification");
+                if (c.length > 0) c[0].remove();
+                var p = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-PrivacyDropdown");
+                if (p.length > 0) p[0].remove();
+                var ht = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-HelpText");
+                if (ht.length > 0) ht[0].remove();
+              }
+            }, 500);
+          }
+        }
+      } else {
+        var sI = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
+        if (sI.length > 0) sI[0].remove();
+        sI = settingsPane.querySelectorAll("#SUITENAV_SITE_INFORMATION");
+        if (sI.length > 0) sI[0].remove();
       }
-    } else {
-      var sI = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
-      if (sI.length > 0) sI[0].remove();
-      sI = settingsPane.querySelectorAll("#SUITENAV_SITE_INFORMATION");
-      if (sI.length > 0) sI[0].remove();
+
+      //var sI2 = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteInformation");
+      //if(sI2.length > 0) sI2[0].remove();
+      // Apply Site Template
+      var sT = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteTemplates();"]');
+      if (sT.length > 0) sT[0].remove();
+      sT = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_WebTempaltesGallery");
+      if (sT.length > 0) sT[0].remove();
+
+      //Site Performance
+      var sP = settingsPane.querySelectorAll('a[href="javascript:_spSitePerformanceScorePage();"]');
+      if (sP.length > 0) sP[0].remove();
+      sP = settingsPane.querySelectorAll("#SUITENAV_SCORE_PAGE");
+      if (sP.length > 0) sP[0].remove();
+
+      // Change the look
+      var cTL = settingsPane.querySelectorAll('a[href="javascript:_spLaunchChangeTheLookPanel();"]');
+      if(cTL.length > 0) cTL[0].remove();
+      cTL = settingsPane.querySelectorAll("#Change_The_Look");
+      if (cTL.length > 0) cTL[0].remove();
+
+      // Schedule Site Launch
+      var sSL = settingsPane.querySelectorAll('a[href="javascript:_spSiteLaunchSchedulerPage();"]');
+      if (sSL.length > 0) sSL[0].remove();
+      sSL = settingsPane.querySelectorAll("#SITE_LAUNCH_SUITENAVID");
+      if (sSL.length > 0) sSL[0].remove();
+
+      // Site Designs
+      var sD = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteDesignProgress();"]');
+      if(sD.length > 0) sD[0].remove();
+      sD = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteDesigns");
+      if(sD.length > 0) sD[0].remove();
     }
-
-    //var sI2 = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteInformation");
-    //if(sI2.length > 0) sI2[0].remove();
-    // Apply Site Template
-    var sT = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteTemplates();"]');
-    if (sT.length > 0) sT[0].remove();
-    sT = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_WebTempaltesGallery");
-    if (sT.length > 0) sT[0].remove();
-
-    //Site Performance
-    var sP = settingsPane.querySelectorAll('a[href="javascript:_spSitePerformanceScorePage();"]');
-    if (sP.length > 0) sP[0].remove();
-    sP = settingsPane.querySelectorAll("#SUITENAV_SCORE_PAGE");
-    if (sP.length > 0) sP[0].remove();
-
-    // Change the look
-    var cTL = settingsPane.querySelectorAll('a[href="javascript:_spLaunchChangeTheLookPanel();"]');
-    if(cTL.length > 0) cTL[0].remove();
-    cTL = settingsPane.querySelectorAll("#Change_The_Look");
-    if (cTL.length > 0) cTL[0].remove();
-
-    // Schedule Site Launch
-    var sSL = settingsPane.querySelectorAll('a[href="javascript:_spSiteLaunchSchedulerPage();"]');
-    if (sSL.length > 0) sSL[0].remove();
-    sSL = settingsPane.querySelectorAll("#SITE_LAUNCH_SUITENAVID");
-    if (sSL.length > 0) sSL[0].remove();
-
-    // Site Designs
-    var sD = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteDesignProgress();"]');
-    if(sD.length > 0) sD[0].remove();
-    sD = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteDesigns");
-    if(sD.length > 0) sD[0].remove();
   }
 }
