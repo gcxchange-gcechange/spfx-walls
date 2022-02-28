2c6eccc0e82202ee18791ffb8bfaee106c863f2c
doug0102 <shea.wow@hotmail.com>
2022-02-28 10:53:45 -0500

    Use CSS instead of binding to events in JS.

--- src/extensions/walls/WallsApplicationCustomizer.ts
+++ src/extensions/walls/WallsApplicationCustomizer.ts
@@ -38,16 +38,12 @@ export default class WallsApplicationCustomizer
   extends BaseApplicationCustomizer<IWallsApplicationCustomizerProperties> {
 
   private userType: userType;
-  private isSettingsOpen: boolean = false; // IDE may say this isn't used but it is.
-  private isMobile: boolean = false;
 
   @override
   public async onInit(): Promise<void> {
     this.userType = await this._checkUser();
 
-    if (this.userType != userType.admin) {
-      this.context.application.navigatedEvent.add(this, this._render);
-    }
+    this.addWallsCSS();
 
     return Promise.resolve();
   }
@@ -98,284 +94,23 @@ export default class WallsApplicationCustomizer
     return retVal;
   }
 
-  public _render() {
-    // Setup the debounced tracking for window resize events
-    // We need this to be able to tell if we're in mobile view or not, as the settings buttons are different.
-    window.addEventListener('resize', this._debounce(function(){
-      this.isMobile = this._isMobile();
-    }));
+  public addWallsCSS(): void {
+    let css: string;
 
-    // Wait for settings button to load, then bind to the click event
-    this._awaitSettingsButtonLoad();
-
-    // Site contents page
-    if (this.context.pageContext.site.serverRequestPath === "/_layouts/15/viewlsts.aspx") {
-      window.setTimeout(() => {
-        let commandbar = document.querySelector(".ms-CommandBar-secondaryCommand");
-        let wf = commandbar.querySelectorAll('button[name="Site workflows"]');
-        wf[0].remove();
-        let ss = commandbar.querySelectorAll('button[name="Site settings"]');
-        ss[0].remove();
-      }, 175);
-    }
-  }
-
-  // This function will wait for either the mobile or desktop settings button to load onto the page.
-  public _awaitSettingsButtonLoad() {
-    let interval = setInterval(() => {
-      var settingsButton = document.getElementById('O365_MainLink_Settings');
-
-      // Look for desktop layout
-      if(settingsButton) {
-
-        this._setupEvents(settingsButton);
-        clearInterval(interval);
-      }
-      else {
-        // Check for mobile layout
-        settingsButton = document.getElementById('O365_MainLink_Affordance');
-
-        if(settingsButton) {
-
-          this.isMobile = true;
-          this._setupEvents(settingsButton);
-          clearInterval(interval);
-        }
-      } 
-    }, 100);
-  }
-  
-  // This sets up the event listeners 
-  public _setupEvents(settingsButton: HTMLElement) {
-    var scope = this;
-
-    if(!this.isMobile) {
-
-      // Hide settings for users and member
-      this._setSettingsHidden(settingsButton);
-
-      settingsButton.addEventListener('click', function(){ return scope._settingsButtonClick(scope) });
-
-      this._setCloseButton('O365_MainLink_Help');
-    }
-    else {
-
-      settingsButton.addEventListener('click', function () {
-        var timeout = 0;
-
-        // Give some delay for the drop down menu to load
-        var interval = setInterval(function() {
-          var mobileSettings = document.getElementById('O365_MainLink_Settings_Affordance');
-
-          if(mobileSettings) {
-
-            // Hide settings for users and member
-            scope._setSettingsHidden(mobileSettings.parentElement);
-            
-            mobileSettings.addEventListener('click', function(){ return scope._settingsButtonClick(scope) });
-            
-            scope._setCloseButton('O365_MainLink_Help_Affordance');
-          }
-
-          // Wait up to half a second to find the settings button before clearing the interval
-          if(mobileSettings || timeout++ >= 50) {
-            clearInterval(interval);
-          }
-        }, 10); 
-      });
+    switch(this.userType) {
+      case userType.user:
+      case userType.member:
+        css = '#O365_MainLink_Settings { display: none !important; } #O365_MainLink_Affordance { display: none !important; } #FlexPane_Settings { display: none !important; }';
+        break;
+      case userType.owner:
+        css = '#SuiteMenu_zz5_MenuItemCreate { display: none !important; } #GLOBALNAV_SETTINGS_SUITENAVID { display: none !important; } #SUITENAV_HUB_SETTINGS { display: none !important; } .ms-SiteSettingsPanel-HelpText a[href*="layouts/15/settings.aspx"] { display: none !important; } .ms-SiteSettingsPanel-classification { display: none !important; } #SUITENAV_SITE_PERMISSIONS { display: none !important; } #SuiteMenu_MenuItem_WebTempaltesGallery { display: none !important; } #SUITENAV_SCORE_PAGE { display: none !important; } #SITE_LAUNCH_SUITENAVID { display: none !important; } #CHANGE_THE_LOOK { display: none !important; }';
+        break;
+      case userType.admin:
+      default:
+        css = '';
+        break;
     }
 
-    // These are the buttons that share open/close state control with the settings pane regardless of mobile layout
-    this._setCloseButton('TipsNTricksButton');
-    this._setCloseButton('O365_MainLink_Me');
-  }
-
-
-  public _awaitSettingsPaneLoad() {
-    var interval = setInterval(() => {
-      var settingsPane = document.getElementById('SettingsFlexPane');
-
-      if(settingsPane) {
-        
-        // Remove links
-        this._addWalls();
-        this._setCloseButton('flexPaneCloseButton');
-
-        clearInterval(interval);
-      }
-    }, 5); // Small interval since this will only be called when the pane is in the process of being loaded
-  }
-
-  public _settingsButtonClick(scope) {
-    scope.isSettingsOpen = !scope.isSettingsOpen;
-
-    if(scope.isSettingsOpen) {
-      scope._awaitSettingsPaneLoad();
-    }
-  }
-
-  public _setSettingsHidden(settingsButton: HTMLElement) {
-    if(this.userType === userType.user || this.userType === userType.member) {
-      settingsButton.style.display = 'none';
-    }
-  }
-
-  // Track the other buttons that automatically close the settings pane.
-  public _setCloseButton(id: string) {
-    var button = document.getElementById(id);
-
-    if(button) {
-      var scope = this;
-
-      button.addEventListener('click', function() {
-        scope.isSettingsOpen = false;
-      });
-    }
-  }
-
-  // Look for the mobile settings button to figure out if we're in the mobile layout or not.
-  public _isMobile(): boolean {
-    var mobileSettings = document.getElementById('O365_MainLink_Affordance');
-
-    // If the layout has changed we need to setup our events again
-    if(mobileSettings && !this.isMobile || !mobileSettings && this.isMobile) {
-      this._awaitSettingsButtonLoad();
-    }
-
-    return mobileSettings ? true : false;
-  }
-
-  // Prevents a function from being called too many times within a given time frame
-  public _debounce(func, timeout = 50){
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => { func.apply(this, args); }, timeout);
-    };
-  }
-
-  public async _addWalls() {
-    // If they're an admin we don't need to remove any settings.
-    if(this.userType === userType.admin) return;
-
-    var settingsPane = document.getElementById('FlexPane_Settings');
-    
-    // Remove options in the settings pane
-    if(settingsPane !== null) {
-
-      // Add page
-      if (this.userType && this.userType != userType.owner) {
-        var aP = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl +'/_layouts/15/CreateSitePage.aspx"]');
-        if (aP && aP.length > 0) aP[0].remove();
-        aP = settingsPane.querySelectorAll("#SuiteMenu_zz8_MenuItemAddPage");
-        if (aP && aP.length > 0) aP[0].remove();
-      }
-
-      //Add app
-      var aP = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl + '/_layouts/15/appStore.aspx#myApps?entry=SettingAddAnApp"]');
-      if (aP && aP.length > 0) aP[0].remove();
-      aP = settingsPane.querySelectorAll("#SuiteMenu_zz5_MenuItemCreate");
-      if (aP && aP.length > 0) aP[0].remove();
-
-      //Global Navigation
-      var gN = settingsPane.querySelectorAll('a[href="javascript:_spLaunchGlobalNavSettings();"]');
-      if (gN && gN.length > 0) gN[0].remove();
-      gN = settingsPane.querySelectorAll("#GLOBALNAV_SETTINGS_SUITENAVID");
-      if (gN && gN.length > 0) gN[0].remove();
-
-      //Hub settings
-      var hS = settingsPane.querySelectorAll('a[href="javascript:_spLaunchHubSettings();"]');
-      if (hS && hS.length > 0) hS[0].remove();
-      hS = settingsPane.querySelectorAll("#SUITENAV_HUB_SETTINGS");
-      if (hS && hS.length > 0) hS[0].remove();
-
-      //Site settings
-      var sT = settingsPane.querySelectorAll('a[href="' + this.context.pageContext.web.serverRelativeUrl + '/_layouts/15/settings.aspx"]');
-      if (sT && sT.length > 0) sT[0].remove();
-      sT = settingsPane.querySelectorAll("#SuiteMenu_zz7_MenuItem_Settings");
-      if (sT && sT.length > 0) sT[0].remove();
-
-      // Site permissions
-      var sP = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSitePermissions();"]');
-      if(sP && sP.length > 0) sP[0].remove();
-      sP = settingsPane.querySelectorAll("#SUITENAV_SITE_PERMISSIONS");
-      if (sP && sP.length > 0) sP[0].remove();
-      sP = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SitePermissions");
-      if (sP && sP.length > 0) sP[0].remove();
-
-      // Site information
-      if (this.userType === userType.owner) {
-        var sI1 = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
-        var sI2 = settingsPane.querySelectorAll('#SuiteMenu_MenuItem_SiteInformation'); //For site content page
-
-        //Check if on home page or site content page
-        if (sI1 && Object.keys(sI1).length > 0) {
-          sI = sI1
-        } else if (sI2 && Object.keys(sI2).length > 0) {
-          sI = sI2
-        }
-
-        if (sI && sI.length > 0) {
-          let element: HTMLElement = sI[0] as HTMLElement;
-          element.onclick = () => {
-            window.setTimeout(() => {
-              var siteSettingsPane = document.getElementsByClassName("ms-SiteSettingsPanel-SiteInfo");
-              if (siteSettingsPane && siteSettingsPane.length > 0) {
-                window.setTimeout(() => {
-                  var jhs = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-joinHubSite");
-                  if (jhs && jhs.length > 0) jhs[0].remove();
-                }, 300);
-                var c = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-classification");
-                if (c && c.length > 0) c[0].remove();
-                var p = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-PrivacyDropdown");
-                if (p && p.length > 0) p[0].remove();
-                var ht = siteSettingsPane[0].getElementsByClassName("ms-SiteSettingsPanel-HelpText");
-                if (ht && ht.length > 0) ht[0].remove();
-              }
-            }, 500);
-          }
-        }
-      } else {
-        var sI = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteSettings();"]');
-        if (sI && sI.length > 0) sI[0].remove();
-        sI = settingsPane.querySelectorAll("#SUITENAV_SITE_INFORMATION");
-        if (sI && sI.length > 0) sI[0].remove();
-      }
-
-      //var sI2 = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteInformation");
-      //if(sI2.length > 0) sI2[0].remove();
-      // Apply Site Template
-      var sT = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteTemplates();"]');
-      if (sT && sT.length > 0) sT[0].remove();
-      sT = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_WebTempaltesGallery");
-      if (sT && sT.length > 0) sT[0].remove();
-
-      //Site Performance
-      var sP = settingsPane.querySelectorAll('a[href="javascript:_spSitePerformanceScorePage();"]');
-      if (sP && sP.length > 0) sP[0].remove();
-      sP = settingsPane.querySelectorAll("#SUITENAV_SCORE_PAGE");
-      if (sP && sP.length > 0) sP[0].remove();
-
-      // Change the look
-      var cTL = settingsPane.querySelectorAll('a[href="javascript:_spLaunchChangeTheLookPanel();"]');
-      if(cTL && cTL.length > 0) cTL[0].remove();
-      cTL = settingsPane.querySelectorAll("#Change_The_Look");
-      if (cTL && cTL.length > 0) cTL[0].remove();
-
-      // Schedule Site Launch
-      var sSL = settingsPane.querySelectorAll('a[href="javascript:_spSiteLaunchSchedulerPage();"]');
-      if (sSL && sSL.length > 0) sSL[0].remove();
-      sSL = settingsPane.querySelectorAll("#SITE_LAUNCH_SUITENAVID");
-      if (sSL && sSL.length > 0) sSL[0].remove();
-
-      // Site Designs
-      var sD = settingsPane.querySelectorAll('a[href="javascript:_spLaunchSiteDesignProgress();"]');
-      if(sD && sD.length > 0) sD[0].remove();
-      sD = settingsPane.querySelectorAll("#SuiteMenu_MenuItem_SiteDesigns");
-      if(sD && sD.length > 0) sD[0].remove();
-    }
-    else {
-      console.error('Walls Extension: Couldn\'t find the settings pane.');
-    }
+    document.head.insertAdjacentHTML('beforeend', '<style>' + css + '</style>');
   }
 }
